755851cc4ffd4a2dea378572bdadda9d54e404c1
誠恩 黃 <108111110@mail.aeust.edu.tw>
2021-11-19 19:06:03 +0800

    北部_南部

--- 110-1HW4/DropdownListShow.aspx.cs
+++ 110-1HW4/DropdownListShow.aspx.cs
@@ -11,7 +11,8 @@ namespace _110_1HW4
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (IsPostBack == false) {
+            if (IsPostBack == false)
+            {
                 ListItem o_Li = new ListItem("北區", "北區");
                 ddl_Area.Items.Add(o_Li);
 
@@ -28,10 +29,12 @@ namespace _110_1HW4
 
         }
 
-        protected void ddl_Area_SelectedIndexChanged(object sender, EventArgs e) {
+        protected void ddl_Area_SelectedIndexChanged(object sender, EventArgs e)
+        {
             ddl_Place.Items.Clear();
             ListItem o_Li;
-            if (ddl_Area.SelectedValue == "北區"){
+            if (ddl_Area.SelectedValue == "北區")
+            {
                 o_Li = new ListItem("基隆", "基隆");
                 ddl_Place.Items.Add(o_Li);
                 o_Li = new ListItem("台北", "台北");
@@ -39,7 +42,8 @@ namespace _110_1HW4
                 o_Li = new ListItem("新北", "新北");
                 ddl_Place.Items.Add(o_Li);
             }
-            else{
+            else
+            {
                 o_Li = new ListItem("苗栗", "苗栗");
                 ddl_Place.Items.Add(o_Li);
                 o_Li = new ListItem("台中", "台中");
@@ -49,4 +53,4 @@ namespace _110_1HW4
             }
         }
     }
- }
+}
